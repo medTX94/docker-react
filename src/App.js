8b2a7522708d2f7da2e88942129d405c5deace11
Bouchlaghem <med@bouchlaghem.com>
2023-06-12 02:41:18 +0000

    initial comit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         that how react works bro instant changes no need to reload or somthing src/App.js and save to realod humm.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
